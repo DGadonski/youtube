378ea9a6e04960c36491f1a95d5066ccc4b9e7e9
DGadonski <derek.gadonski@gmail.com>
2023-08-23 13:32:07 -0300

    Changes in 'pubspec' and 'Inicio'.

--- lib/telas/Inicio.dart
+++ lib/telas/Inicio.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import '../Api.dart';
+
 class Inicio extends StatefulWidget {
   const Inicio({super.key});
 
@@ -10,8 +12,13 @@ class Inicio extends StatefulWidget {
 }
 
 class _InicioState extends State<Inicio> {
+
   @override
   Widget build(BuildContext context) {
+
+    Api api = Api();
+    api.pesquisar('');    
+
     return Container(
       child: Center(
         child: Text('Inicio'),
